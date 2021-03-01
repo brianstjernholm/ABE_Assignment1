162d035db6de2299c703411fb98b931ec4436691
brianstjernholm <brianstjernholm@gmail.com>
2021-03-01 10:16:31 +0100

    seting up heroku

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var path = require('path')
+var jwt = require('jsonwebtoken');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+const User = require('./models/user')
+
+require("dotenv").config({
+  path: path.join(__dirname, "../.env")
+ });
 
 require('./models/db');
 
@@ -25,6 +33,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hotel', hotelRouter);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+app.use(async (req, res, next) => {
+ if (req.headers["x-access-token"]) {
+  const accessToken = req.headers["x-access-token"];
+  const { userId, exp } = await jwt.verify(accessToken, process.env.JWT_SECRET);
+  // Check if token has expired
+  if (exp < Date.now().valueOf() / 1000) { 
+   return res.status(401).json({ error: "JWT token has expired, please login to obtain a new one" });
+  } 
+  res.locals.loggedInUser = await User.findById(userId); next(); 
+ } else { 
+  next(); 
+ } 
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
